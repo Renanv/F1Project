2217a5fb01c72e85a3780de5191f374f91fb67be
Renanv <renanboarin@hotmail.com>
2025-05-04 16:10:32 +0200

    change port

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const helmet = require('helmet');
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(helmet());
