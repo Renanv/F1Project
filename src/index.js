05dc7f4a3452c8483fe07454a8907a543808ad85
Renanv <renanboarin@hotmail.com>
2025-05-21 10:02:09 +0200

    Logo + service worker

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const theme = createTheme({
   palette: {
@@ -28,3 +29,20 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+serviceWorkerRegistration.register({
+  onUpdate: registration => {
+    const waitingServiceWorker = registration.waiting;
+    if (waitingServiceWorker) {
+      if (window.confirm("A new version of the app is available. Refresh to update?")) {
+        waitingServiceWorker.postMessage({ type: 'SKIP_WAITING' });
+        // Optional: Listen for controller change to reload once the new SW is active
+        navigator.serviceWorker.addEventListener('controllerchange', () => {
+          window.location.reload();
+        });
+      } else {
+        console.log("User declined to update to the new version immediately.");
+      }
+    }
+  }
+});
