ab1875e5a7d3e04172813c4e1b8483975a791a91
Renanv <renanboarin@hotmail.com>
2025-05-28 14:30:34 +0200

    Change LSF Score to F1 25

--- src/components/rankings/ConstructorsRankingsView.js
+++ src/components/rankings/ConstructorsRankingsView.js
@@ -6,15 +6,15 @@ import {
 
 // Define Constructor Tiers - This should be the same as in RankingsPage.js or imported if centralized
 const constructorTiers = [
-    { ranks: [1, 2], name: "Red Bull", color: '#0600EF' },
-    { ranks: [3, 4], name: "McLaren", color: '#FF8700' },
-    { ranks: [5, 6], name: "Ferrari", color: '#DC0000' },
-    { ranks: [7, 8], name: "Mercedes", color: '#00D2BE' },
+    { ranks: [1, 2], name: "McLaren", color: '#FF8700' },
+    { ranks: [3, 4], name: "Red Bull", color: '#0600EF' },
+    { ranks: [5, 6], name: "Mercedes", color: '#00D2BE' },
+    { ranks: [7, 8], name: "Ferrari", color: '#DC0000' },
     { ranks: [9, 10], name: "Aston Martin", color: '#006F62' },
-    { ranks: [11, 12], name: "Alpine", color: '#0090FF' },
+    { ranks: [11, 12], name: "Williams", color: '#005AFF' },
     { ranks: [13, 14], name: "VCARB", color: '#00BFFF' },
-    { ranks: [15, 16], name: "Haas", color: '#FFFFFF', textColor: '#000000' },
-    { ranks: [17, 18], name: "Williams", color: '#005AFF' },
+    { ranks: [15, 16], name: "Alpine", color: '#0090FF' },
+    { ranks: [17, 18], name: "Haas", color: '#FFFFFF', textColor: '#000000' },
     { ranks: [19, 20], name: "Sauber", color: '#00E100' }
 ];
 
